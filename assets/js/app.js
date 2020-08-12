af08ccab0093ce9cd69204fb1cca0bf3b8dfb92b
Fred Gryson <fgryson@gmail.com>
2020-08-12 16:41:44 +0100

    Displaying Videos. As per Pg 192

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,14 +2,13 @@
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
 import "../css/app.scss"
-
-// webpack automatically bundles all modules in your
-// entry points. Those entry points can be configured
-// in "webpack.config.js".
-//
-// Import deps with the dep name or local files with a relative path, for example:
-//
-//     import {Socket} from "phoenix"
-//     import socket from "./socket"
-//
 import "phoenix_html"
+import Player from "./player"
+
+let video = document.getElementById("video")
+
+if(video) {
+    Player.init(video.id, video.getAttribute("data-player-id"), () => {
+        console.log("player ready!")
+    })
+}
